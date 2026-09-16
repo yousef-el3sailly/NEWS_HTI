f87e6875e8cc193fced9ff876a228e4055c4e3ee
yousef-el3sailly <jeisaily@gmail.com>
2026-09-16 14:34:04 +0300

    Fix social preview image

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -102,11 +102,11 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       },
       {
         property: "og:image",
-        content: "https://news-hti.vercel.app/og-image.png",
+        content: "https://news-hti.vercel.app/news-logo.png",
       },
       {
         property: "og:image:secure_url",
-        content: "https://news-hti.vercel.app/og-image.png",
+        content: "https://news-hti.vercel.app/news-logo.png",
       },
       {
         property: "og:image:type",
@@ -130,7 +130,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       },
       {
         name: "twitter:image",
-        content: "https://news-hti.vercel.app/og-image.png",
+        content: "https://news-hti.vercel.app/news-logo.png",
       },
     ],
     links: [
